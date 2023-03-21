ade82becdfcb4847d3fbbd866b1cc8ba7e58d2dc
hussien tawhidi <hussientawhidi710@gmail.com>
2023-03-21 17:53:59 +0330

    hussien tawhidi

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaFacebookF,
+  FaYoutube,
+  FaTwitch,
+  FaInstagram,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className='stick_follow_icon'>
+      <ul>
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a href={socialprofils.facebook}>
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>On Social Media</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,192 @@
+const logotext = "Hussien";
+
+const meta = {
+  title: "Hussien Tawhidi",
+  description: "I’m John Hussien Tawhidi _ Web-developer",
+};
+
+const introdata = {
+  title: "I’m Hussien Tawhidi",
+  animated: {
+    first: "I love coding",
+    second: "I code cool design",
+    third: "I develop mobile apps",
+  },
+  description: "",
+};
+
+const dataabout = {
+  title: "Abit about my self",
+  aboutme:
+    "In general, I had a turbulent life ahead of me and I always faced many ups and downs. Even many times these obstacles made me drop out of education for a while. I got up again and continued. And I will continue until I can reach my main goal.promised my life that I will definitely reach my goals",
+};
+
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "html",
+    value: 90,
+  },
+  {
+    name: "css",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "nodejs",
+    value: 50,
+  },
+  {
+    name: "photoshop",
+    value: 70,
+  },
+  {
+    name: "after effect",
+    value: 40,
+  },
+];
+
+const services = [
+  {
+    title: "Web Development(front-end)",
+    description:
+      "I try to design the website in the best possible way because it is one of my favorite things and my hobby.I design the website with html css reactjs bootstrap and some more features.In website design, I try not to use too many colors, I mostly tell my publishers to try to use one or at least two colors as their brand.",
+  },
+  {
+    title: "Web Development(backend)",
+    description:
+      "It forms the most important part of a website. It considers all the security of the site and the user. For now, I use nodejs, but later I will plan to use Python for the use of artificial intelligence",
+  },
+  {
+    title: "UI & UX Design",
+    description:
+      "In general, I have a good knowledge of design and I can design posters and banners, logos or even complete branding. I can work with Photoshop, After Effects and Premiere, but I work mostly with Photoshop",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "hussientawhidi710@gmail.com",
+  YOUR_FONE: "(+98) 903 093 1288",
+  description:
+    "You can contact IMO WhatsApp or Telegram at the above number, or you can send an email to my email address or fill out the form, and then I will contact you personally, thank you very much.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_zacvcmq",
+  YOUR_TEMPLATE_ID: "template_alpu8ka",
+  YOUR_USER_ID: "LuxJBqpAMD_htyoGR",
+};
+
+const socialprofils = {
+  github: "https://github.com/hussien-tawhidi",
+  facebook: "https://www.facebook.com/hossien.tawhidi",
+  instagram: "https://www.instagram.com/h_tawhidi/",
+  youtube: "https://www.youtube.com/channel/UCc30PTxXlyPf0f26SuKV52Q",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
