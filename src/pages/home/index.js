313668b7dd2ec41db99b9b2b49a7cb71b235a53d
hussien tawhidi <hussientawhidi710@gmail.com>
2023-03-22 01:53:03 +0430

    btn updated

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -39,7 +39,7 @@ export const Home = () => {
                 </h1>
                 <p className='mb-1x'>{introdata.description}</p>
                 <div className='intro_btn-action pb-5'>
-                  <Link to='/portfolio' className='text_2 mb-4'>
+                  <Link to='/portfolio' className='text_2'>
                     <div id='button_p' className='ac_btn btn '>
                       My Portfolio
                       <div className='ring one'></div>
@@ -48,7 +48,7 @@ export const Home = () => {
                     </div>
                   </Link>
                   <Link to='/contact'>
-                    <div id='button_h' className='ac_btn btn'>
+                    <div id='button_h' className='ac_btn btn mt-4'>
                       Contact Me
                       <div className='ring one'></div>
                       <div className='ring two'></div>
