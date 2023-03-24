cc9f03c8dfa6a76050edb1fccf7bcb503f6a4af6
hussien tawhidi <hussientawhidi710@gmail.com>
2023-03-25 01:06:34 +0430

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -22,7 +22,7 @@ export const Home = () => {
         <div className='intro_sec d-block d-lg-flex align-items-center '>
           <div
             className='h_bg-image order-1 order-lg-2 h-100 h-sm-50'
-            style={{ backgroundImage: `url${profileImg}` }}></div>
+            style={{ backgroundImage: `url(${profileImg})` }}></div>
           <div className='text order-2 order-lg-1 h-100 d-lg-flex justify-content-center '>
             <div className='align-self-center '>
               <div className='intro mx-auto'>
